b0ea32096f27a49fac60f9efae451032c5c9d9fd
Fabio Galante Mans <fgamans@hotmail.com>
2024-05-02 09:53:31 -0300

    Initial commit

---
+++ BackgroundDemo/Program.cs
@@ -0,0 +1,22 @@
+using BackgroundDemo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<SampleData>();
+builder.Services.AddHostedService<BackgroundRefresh>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/messages", (SampleData data) => data.Data.Order());
+
+app.Run();
